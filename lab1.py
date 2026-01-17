d375c7ad1bebcfbfcc738c8a45c5a7d49b16e4d6
Nilima Barick <nilima.barick98@gmail.com>
2026-01-17 23:30:26 +0530

    lab1 complete

---
+++ lab1.py
@@ -0,0 +1,17 @@
+#hello world
+print("hello world")
+#create a form using input and ouput function
+#1st user name, 2nd user age ,3rd address,
+
+user_name = input("enter your name")
+user_age = int(input("enter your age"))
+user_add = input("enter your addresss")
+print(user_name)
+print(user_age)
+print(user_add)
+
+#check a number whether it is even or odd
+if (user_age % 2 == 0):
+    print("your age is even")
+else:
+    print("your age is odd")
